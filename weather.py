0918e8760e874e58751035a9edf189874fd12d8d
chihiro <chihiro@yandex-team.ru>
2018-12-17 17:59:40 +0300

    обработка исключений

--- weather.py
+++ weather.py
@@ -11,14 +11,19 @@ def weather_by_city(city_name):
         'num_of_days': 1,
         'lang': 'ru'
     }
-    result = requests.get(weather_url, params=params)
-    weather = result.json()
-    if 'data' in weather:
-        if 'current_condition' in weather['data']:
-            try:
-                return weather['data']['current_condition'][0]
-            except(IndexError, TypeError):
-                return False
+    try:
+        result = requests.get(weather_url, params=params)
+        result.raise_for_status()
+        weather = result.json()
+        if 'data' in weather:
+            if 'current_condition' in weather['data']:
+                try:
+                    return weather['data']['current_condition'][0]
+                except(IndexError, TypeError):
+                    return False
+    except(requests.RequestException, ValueError):
+        print('Сетевая ошибка')
+        return False
     return False
 
 
